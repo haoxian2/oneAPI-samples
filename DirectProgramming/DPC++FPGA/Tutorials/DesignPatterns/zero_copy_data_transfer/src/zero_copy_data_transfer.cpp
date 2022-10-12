429579a9820de367b93f56d6eb7af9785392fb02
Yang, Hao Xiang <hao.xiang.yang@intel.com>
2022-10-12 13:08:19 -0700

    commit to rebase

--- DirectProgramming/DPC++FPGA/Tutorials/DesignPatterns/zero_copy_data_transfer/src/zero_copy_data_transfer.cpp
+++ DirectProgramming/DPC++FPGA/Tutorials/DesignPatterns/zero_copy_data_transfer/src/zero_copy_data_transfer.cpp
@@ -27,6 +27,9 @@ int main(int argc, char* argv[]) {
 #if defined(FPGA_EMULATOR)
   size_t size = 10000;
   size_t iterations = 1;
+#elif defined(FPGA_SIMULATOR)
+  size_t size = 10000;
+  size_t iterations = 1;
 #else
   size_t size = 100000000;
   size_t iterations = 5;
@@ -47,6 +50,8 @@ int main(int argc, char* argv[]) {
     // device selector
 #if defined(FPGA_EMULATOR)
     ext::intel::fpga_emulator_selector selector;
+#elif defined(FPGA_SIMULATOR)
+    ext::intel::fpga_simulator_selector selector;
 #else
     ext::intel::fpga_selector selector;
 #endif
